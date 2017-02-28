00fe5286d521d9ba40842e7dba771b915ca53c7b
Adam Reed <kusa.xisa@gmail.com>
2017-02-28 15:07:57 -0500

    Integrated rails with angular

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,31 @@
+// ['ui.router', 'templates']
+(function(ng) {
+	"use strict";
+
+	ng.module('sixStringApp', ['ui.router']);
+	ng.module('sixStringApp').config(function($stateProvider, $urlRouterProvider) {
+
+		$urlRouterProvider.otherwise('/'); // if url does not exist
+		//..  use parent
+		$urlRouterProvider.when('/', '/' + 'login');
+		$stateProvider.state('gameParent', {
+			url: '/', // base parent
+			abstract: true, // will contain other templates inside of this state. Not a template itself
+			template: '<ui-view></ui-view>' // placed the tags into the index where all the templates will appear
+			// you can make multiple views by using 'names in ui-view tag '
+		}).state('gameParent.loginCtrl', {
+			url: 'login',
+			controller: 'LoginController as logCtrl',
+			templateUrl: './templates/login.html'
+		}).state('gameParent.gameCtrl', {
+			url: 'game',
+			controller: 'GameController as gameCtrl',
+			templateUrl: './templates/game.html'
+		}).state('gameParent.optCtrl', {
+			url: 'options',
+			controller: 'OptionsController as optCtrl',
+			templateUrl: './templates/options.html'
+		});
+	});
+
+})(angular);
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # get 'index/show'
+  #
+  root 'index#show'
+end
