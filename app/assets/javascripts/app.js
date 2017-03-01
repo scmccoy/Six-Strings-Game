05766ad5df4c2a480ebb8c4d97eb1cb91ae79049
Vallyre Hyers <vallyre@vallyre.com>
2017-02-28 19:58:43 -0500

    Added controller functionality

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,28 +3,35 @@
 	"use strict";
 
 	ng.module('sixStringApp', ['ui.router', 'templates']);
+
 	ng.module('sixStringApp').config(function($stateProvider, $urlRouterProvider) {
 
-		$urlRouterProvider.otherwise('/'); // if url does not exist
-		//..  use parent
-		$urlRouterProvider.when('/', '/' + 'login');
+		$urlRouterProvider.otherwise('/');
+		// $urlRouterProvider.when('/', '/' + 'login');
 		$stateProvider.state('gameParent', {
-			url: '/', // base parent
-			abstract: true, // will contain other templates inside of this state. Not a template itself
-			template: '<ui-view></ui-view>' // placed the tags into the index where all the templates will appear
-			// you can make multiple views by using 'names in ui-view tag '
+			url: '/',
+			abstract: true,
+			template: '<ui-view></ui-view>'
 		}).state('gameParent.loginCtrl', {
 			url: 'login',
 			controller: 'LoginController as logCtrl',
 			templateUrl: 'login.html'
+		}).state('gameParent.startCtrl', {
+			url: 'start',
+			controller: 'StartController as startCtrl',
+			templateUrl: 'start.html'
 		}).state('gameParent.gameCtrl', {
 			url: 'game',
 			controller: 'GameController as gameCtrl',
 			templateUrl: 'game.html'
-		}).state('gameParent.optCtrl', {
-			url: 'options',
-			controller: 'OptionsController as optCtrl',
-			templateUrl: 'options.html'
+		}).state('gameParent.ldbdCtrl', {
+			url: 'leaderboard',
+			controller: 'LeaderboardController as ldbdCtrl',
+			templateUrl: 'leaderboard.html'
+		}).state('gameParent.winCtrl', {
+			url: 'win',
+			controller: 'WinController as winCtrl',
+			templateUrl: 'win.html'
 		});
 	});
 
