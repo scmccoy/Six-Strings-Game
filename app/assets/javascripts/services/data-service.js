82625e6fa37d9c7b772d180a539cc01d1ec3a740
Shane <shanemccoypdx@gmail.com>
2017-03-01 21:33:08 -0500

    updated service, start ctrl

--- app/assets/javascripts/services/data-service.js
+++ app/assets/javascripts/services/data-service.js
@@ -2,8 +2,50 @@
 	"use strict";
 
 	ng.module('sixStringApp').service('dataService', function() {
+		function AllDataService($http) {
+			function getData(url) {
+				return $http({
+					method: 'GET',
+					url: url
+				});
+			}
 
+			function postData(url) {
+				return $http({
+					method: 'POST',
+					url: url,
+					headers: {
+						"content-type": "application/json;charset=utf-8"
+					}
+					// data: JSON.stringify({
+					//
+					// })
+				});
+			}
 
+			function putData(url) {
+				return $http({
+					method: 'PUT',
+					url: url
+				});
+			}
+
+			function deleteData(url) {
+				return $http({
+					method: 'DELETE',
+					url: url,
+					headers: {
+						"content-type": "application/json;charset=utf-8"
+					}
+				});
+			}
+
+			return {
+				get: getData,
+				post: postData,
+				put: putData,
+				delete: deleteData
+			};
+		}
 	});
-
 })(angular);
