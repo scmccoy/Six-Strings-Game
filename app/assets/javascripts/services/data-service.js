9e4bfb4f17822411a054fde590cf6dc433627ce3
Adam Reed <kusa.xisa@gmail.com>
2017-03-01 20:05:54 -0500

    Removed .js files for heroku test

--- app/assets/javascripts/services/data-service.js
+++ app/assets/javascripts/services/data-service.js
@@ -1,41 +1,41 @@
-(function(ng) {
-	"use strict";
-
-	ng.module('sixStringApp').service('dataService', function($http) {
-
-
-		/////////////////////////////////////
-		// API :: GET  // FOR TESTING ALL GET RESPONSES
-		/////////////////////////////////////
-		function getChecker() {
-			const p1 = $.get(`https://##`);
-			// const p2 = $.get(`https://##`);
-			// const p3 = $.get(`https://##`);
-			Promise.all([p1]).then((results) => {
-				console.log(results);
-			}).catch((error) => {
-				console.log(error);
-			});
-		} // END GET CHECKER
-
-		/////////////////////////////////////
-		// API :: POST INPUT
-		/////////////////////////////////////
-
-
-		/////////////////////////////////////
-		// API :: GET : WORDS
-		/////////////////////////////////////
-		function getWords(url) {
-
-			return $http({
-				method: 'GET',
-				url: url
-			});
-		}
-		return {
-			get: getWords
-		};
-		// END START GAME CALL
-	});
-})(angular);
+// (function(ng) {
+// 	"use strict";
+//
+// 	ng.module('sixStringApp').service('dataService', function($http) {
+//
+//
+// 		/////////////////////////////////////
+// 		// API :: GET  // FOR TESTING ALL GET RESPONSES
+// 		/////////////////////////////////////
+// 		function getChecker() {
+// 			const p1 = $.get(`https://##`);
+// 			// const p2 = $.get(`https://##`);
+// 			// const p3 = $.get(`https://##`);
+// 			Promise.all([p1]).then((results) => {
+// 				console.log(results);
+// 			}).catch((error) => {
+// 				console.log(error);
+// 			});
+// 		} // END GET CHECKER
+//
+// 		/////////////////////////////////////
+// 		// API :: POST INPUT
+// 		/////////////////////////////////////
+//
+//
+// 		/////////////////////////////////////
+// 		// API :: GET : WORDS
+// 		/////////////////////////////////////
+// 		function getWords(url) {
+//
+// 			return $http({
+// 				method: 'GET',
+// 				url: url
+// 			});
+// 		}
+// 		return {
+// 			get: getWords
+// 		};
+// 		// END START GAME CALL
+// 	});
+// })(angular);
