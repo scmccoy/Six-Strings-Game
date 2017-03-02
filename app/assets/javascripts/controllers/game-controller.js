3578d802a4b7af4620b754927151b1b0e7347bb6
Shane <shanemccoypdx@gmail.com>
2017-03-02 08:00:49 -0500

    testing js commented out for now. prepping data service

--- app/assets/javascripts/controllers/game-controller.js
+++ app/assets/javascripts/controllers/game-controller.js
@@ -1,10 +1,16 @@
 (function(ng) {
 	"use strict";
 
-	ng.module('sixStringApp').controller('GameController', function() {
+	ng.module('sixStringApp').controller('GameController', function(dataService, $q, $state, $scope) {
 		console.log('in GameController');
 
-		
+		$q.when(DataService.get('./puzzles')).then((response) => {
+			this.getPuzzle = response;
+			console.log(this.getPuzzle);
+		}).catch((error) => {
+			console.log(error);
+		});
+
 	});
 
 })(angular);
--- app/assets/javascripts/testing.js
+++ app/assets/javascripts/testing.js
@@ -1,214 +1,208 @@
-// test only - delete for production
-const words = ['tiger', 'sheep', 'badger', 'giraffe', 'platypus', 'porcupine'];
-//words. 5x2, 1x6, 1x7, 1x8 & 1x9
-const clues = ['this word has five letters', 'this word has five letters too', 'this word has six letters', 'this word has seven letters', 'this word has eight letters', 'this word has nine letters'];
-
-const round = {
-  "NOBLE":"Periodic table gas",
-  "OUSTS":"Removes from power",
-  "OTHERS":"Not yourself",
-  "SECULAR":"Not religious",
-  "BUBBLES":"Oxygen in water",
-  "KNOWLEDGE":"School gains"
-};
-
-let word = 'NOBLE';
-
-function modulusLength(round) {
-  Object.keys(round).forEach(function(i) {
-    if ((i % 2) == 1) {
-    }
-
-});
-}
-
-modulusLength(round);
-
-
-
-
-
-
-
-
-
-
-
-
-
-//clues - x6. Has number in each to mark
-///////////////////////////////////////
-//**
-=======
-let mixedParts = [];
-let mixedClues = [];
-const wordClue = {
-	"NOBLE": "Periodic table gas",
-	"OUSTS": "Removes from power",
-	"OTHERS": "Not yourself",
-	"SECULAR": "Not religious",
-	"BUBBLES": "Oxygen in water",
-	"KNOWLEDGE": "School gains"
-};
-
-function modulusLength(wordClue) {
-	Object.keys(wordClue).forEach(function(i) {
-		// if ((i % 2) == 1) {}
-		if (i.length === 5) {
-			wordCountFive(i);
-		} else if (i.length === 6) {
-			wordCountSix(i);
-		} else if (i.length === 7) {
-			wordCountSeven(i);
-		} else if (i.length === 8) {
-			wordCountEight(i);
-		} else if (i.length === 9) {
-			wordCountNine(i);
-		} else {
-			console.log('Error in wordBreaker');
-		}
-		mixedClues.push(wordClue[i]);
-		// console.log('key ', i);
-		// console.log('value', wordClue[i]);
-	});
-}
-
-///////////////////////////////////////
-//** RANDOM FOR TRUE/FALSE
-///////////////////////////////////////
-//** 50% chance of true / false
-function randomTruth() {
-	if (Math.random() > 0.5) {
-		return true;
-	} else {
-		return false;
-	}
-}
-<<<<<<< HEAD
-//** Sum of an Array (all 6 words)
-let total = 0;
-
-function getSum(numbers) {
-	for (let i = 0; i < numbers.length; i++)
-		total += numbers[i];
-	console.log('total', total);
-	return total;
-}
-//** length of array items to be added in getSum function
-let letterCount = []; //
-function stuff(wordArray) {
-	for (let i = 0; i < wordArray.length; i++) {
-		letterCount.push(wordArray[i].length);
-		console.log('letterCount --> ', letterCount);
-	}
-
-}
-stuff(words); // counts length of strings in array items
-getSum(letterCount); // sum of string lengths from array
-// const words = ['fiveM', 'five2', 'sixMMM', 'sevenMM', 'eightMMM', 'nineMMMMM'];
-let mixedParts = []; // array for word sections ***********************IMPORTANT
-
-function wordBreaker(wordChooser) {
-	console.log('wordChooser', wordChooser);
-	for (let j = 0; j < wordChooser.length; j++) {
-		if (wordChooser[j].length === 5) {
-			wordCountFive(wordChooser[j]);
-		} else if (wordChooser[j].length === 6) {
-			wordCountSix(wordChooser[j]);
-		} else if (wordChooser[j].length === 7) {
-			wordCountSeven(wordChooser[j]);
-		} else if (wordChooser[j].length === 8) {
-			wordCountEight(wordChooser[j]);
-		} else if (wordChooser[j].length === 9) {
-			wordCountNine(wordChooser[j]);
-		} else {
-			console.log('Error in wordBreaker');
-		}
-	}
-}
-
-
-// function randomTruth() { // testing copy
+// // // test only - delete for production
+// // const words = ['tiger', 'sheep', 'badger', 'giraffe', 'platypus', 'porcupine'];
+// // //words. 5x2, 1x6, 1x7, 1x8 & 1x9
+// // const clues = ['this word has five letters', 'this word has five letters too', 'this word has six letters', 'this word has seven letters', 'this word has eight letters', 'this word has nine letters'];
+// //
+// // const round = {
+// //   "NOBLE":"Periodic table gas",
+// //   "OUSTS":"Removes from power",
+// //   "OTHERS":"Not yourself",
+// //   "SECULAR":"Not religious",
+// //   "BUBBLES":"Oxygen in water",
+// //   "KNOWLEDGE":"School gains"
+// // };
+// //
+// // let word = 'NOBLE';
+// //
+// // function modulusLength(round) {
+// //   Object.keys(round).forEach(function(i) {
+// //     if ((i % 2) == 1) {
+// //     }
+// //
+// // });
+// // }
+// //
+// // modulusLength(round);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// //clues - x6. Has number in each to mark
+// ///////////////////////////////////////
+// //**
+//
+// let mixedParts = [];
+// let mixedClues = [];
+// const wordClue = {
+// 	"NOBLE": "Periodic table gas",
+// 	"OUSTS": "Removes from power",
+// 	"OTHERS": "Not yourself",
+// 	"SECULAR": "Not religious",
+// 	"BUBBLES": "Oxygen in water",
+// 	"KNOWLEDGE": "School gains"
+// };
+//
+// function modulusLength(wordClue) {
+// 	Object.keys(wordClue).forEach(function(i) {
+// 		// if ((i % 2) == 1) {}
+// 		if (i.length === 5) {
+// 			wordCountFive(i);
+// 		} else if (i.length === 6) {
+// 			wordCountSix(i);
+// 		} else if (i.length === 7) {
+// 			wordCountSeven(i);
+// 		} else if (i.length === 8) {
+// 			wordCountEight(i);
+// 		} else if (i.length === 9) {
+// 			wordCountNine(i);
+// 		} else {
+// 			console.log('Error in wordBreaker');
+// 		}
+// 		mixedClues.push(wordClue[i]);
+// 		// console.log('key ', i);
+// 		// console.log('value', wordClue[i]);
+// 	});
+// }
+//
+// ///////////////////////////////////////
+// //** RANDOM FOR TRUE/FALSE
+// ///////////////////////////////////////
+// //** 50% chance of true / false
+// function randomTruth() {
 // 	if (Math.random() > 0.5) {
 // 		return true;
 // 	} else {
 // 		return false;
 // 	}
-// } // testing copy
-//** Five letter word
-function wordCountFive(fiveLetterWord) {
-	console.log('wordcount 5 Arg ', fiveLetterWord);
-	if (randomTruth()) {
-		mixedParts.push(fiveLetterWord.slice(0, 3));
-		mixedParts.push(fiveLetterWord.slice(3, 5));
-	} else {
-		mixedParts.push(fiveLetterWord.slice(0, 2));
-		mixedParts.push(fiveLetterWord.slice(2, 5));
-	}
-}
-
-//** Six letter word
-function wordCountSix(sixLetterWord) {
-	console.log('wordcount 6 Arg ', sixLetterWord);
-	mixedParts.push(sixLetterWord.slice(0, 2));
-	mixedParts.push(sixLetterWord.slice(2, 4));
-	mixedParts.push(sixLetterWord.slice(4, 6));
-}
-
-//** Seven letter word
-function wordCountSeven(sevenLetterWord) {
-	console.log('wordcount 7 Arg ', sevenLetterWord);
-	if (randomTruth()) {
-		mixedParts.push(sevenLetterWord.slice(0, 2));
-		mixedParts.push(sevenLetterWord.slice(2, 5));
-		mixedParts.push(sevenLetterWord.slice(5, 7));
-	} else {
-		mixedParts.push(sevenLetterWord.slice(0, 3));
-		mixedParts.push(sevenLetterWord.slice(3, 5));
-		mixedParts.push(sevenLetterWord.slice(5, 7));
-	}
-}
-
-//** Eight letter word
-function wordCountEight(eightLetterWord) {
-	console.log('wordcount 8 Arg ', eightLetterWord);
-	if (randomTruth()) {
-		mixedParts.push(eightLetterWord.slice(0, 3));
-		mixedParts.push(eightLetterWord.slice(3, 5));
-		mixedParts.push(eightLetterWord.slice(5, 8));
-	} else {
-		mixedParts.push(eightLetterWord.slice(0, 2));
-		mixedParts.push(eightLetterWord.slice(2, 5));
-		mixedParts.push(eightLetterWord.slice(5, 8));
-	}
-}
-
-//** Nine letter word
-function wordCountNine(nineLetterWord) {
-	console.log('wordcount 9 Arg ', nineLetterWord);
-	mixedParts.push(nineLetterWord.slice(0, 3));
-	mixedParts.push(nineLetterWord.slice(3, 6));
-	mixedParts.push(nineLetterWord.slice(6, 9));
-}
-<<<<<<< HEAD
-wordBreaker(words);
-console.log('mixedparts ', mixedParts);
-//** assigning vars to clues
-//const clues = ['this word has five letters', 'this word has five letters too', 'this word has six letters', 'this word has seven letters', 'this word has eight letters', 'this word has nine letters'];
-let fiveLetterClue = clues[0];
-let fiveLetterClueTwo = clues[1];
-let sixLetterClue = clues[2];
-let sevenLetterClue = clues[3];
-let eightLetterClue = clues[4];
-let nineLetterClue = clues[5];
-
-console.log('5 ltr clue => ', fiveLetterClue);
-console.log('5 ltr clue(2) => ', fiveLetterClueTwo);
-console.log('6 ltr clue => ', sixLetterClue);
-console.log('7 ltr clue => ', sevenLetterClue);
-console.log('8 ltr clue => ', eightLetterClue);
-console.log('9 ltr clue => ', nineLetterClue);
-=======
-modulusLength(wordClue);
-console.log('mixedparts ', mixedParts);
-console.log('mixedClues ', mixedClues);
+// }
+// //** Sum of an Array (all 6 words)
+// let total = 0;
+//
+// function getSum(numbers) {
+// 	for (let i = 0; i < numbers.length; i++)
+// 		total += numbers[i];
+// 	console.log('total', total);
+// 	return total;
+// }
+// //** length of array items to be added in getSum function
+// let letterCount = []; //
+// function stuff(wordArray) {
+// 	for (let i = 0; i < wordArray.length; i++) {
+// 		letterCount.push(wordArray[i].length);
+// 		console.log('letterCount --> ', letterCount);
+// 	}
+//
+// }
+// stuff(words); // counts length of strings in array items
+// getSum(letterCount); // sum of string lengths from array
+// // const words = ['fiveM', 'five2', 'sixMMM', 'sevenMM', 'eightMMM', 'nineMMMMM'];
+// let mixedParts = []; // array for word sections ***********************IMPORTANT
+//
+// function wordBreaker(wordChooser) {
+// 	console.log('wordChooser', wordChooser);
+// 	for (let j = 0; j < wordChooser.length; j++) {
+// 		if (wordChooser[j].length === 5) {
+// 			wordCountFive(wordChooser[j]);
+// 		} else if (wordChooser[j].length === 6) {
+// 			wordCountSix(wordChooser[j]);
+// 		} else if (wordChooser[j].length === 7) {
+// 			wordCountSeven(wordChooser[j]);
+// 		} else if (wordChooser[j].length === 8) {
+// 			wordCountEight(wordChooser[j]);
+// 		} else if (wordChooser[j].length === 9) {
+// 			wordCountNine(wordChooser[j]);
+// 		} else {
+// 			console.log('Error in wordBreaker');
+// 		}
+// 	}
+// }
+//
+//
+// // function randomTruth() { // testing copy
+// // 	if (Math.random() > 0.5) {
+// // 		return true;
+// // 	} else {
+// // 		return false;
+// // 	}
+// // } // testing copy
+// //** Five letter word
+// function wordCountFive(fiveLetterWord) {
+// 	console.log('wordcount 5 Arg ', fiveLetterWord);
+// 	if (randomTruth()) {
+// 		mixedParts.push(fiveLetterWord.slice(0, 3));
+// 		mixedParts.push(fiveLetterWord.slice(3, 5));
+// 	} else {
+// 		mixedParts.push(fiveLetterWord.slice(0, 2));
+// 		mixedParts.push(fiveLetterWord.slice(2, 5));
+// 	}
+// }
+//
+// //** Six letter word
+// function wordCountSix(sixLetterWord) {
+// 	console.log('wordcount 6 Arg ', sixLetterWord);
+// 	mixedParts.push(sixLetterWord.slice(0, 2));
+// 	mixedParts.push(sixLetterWord.slice(2, 4));
+// 	mixedParts.push(sixLetterWord.slice(4, 6));
+// }
+//
+// //** Seven letter word
+// function wordCountSeven(sevenLetterWord) {
+// 	console.log('wordcount 7 Arg ', sevenLetterWord);
+// 	if (randomTruth()) {
+// 		mixedParts.push(sevenLetterWord.slice(0, 2));
+// 		mixedParts.push(sevenLetterWord.slice(2, 5));
+// 		mixedParts.push(sevenLetterWord.slice(5, 7));
+// 	} else {
+// 		mixedParts.push(sevenLetterWord.slice(0, 3));
+// 		mixedParts.push(sevenLetterWord.slice(3, 5));
+// 		mixedParts.push(sevenLetterWord.slice(5, 7));
+// 	}
+// }
+//
+// //** Eight letter word
+// function wordCountEight(eightLetterWord) {
+// 	console.log('wordcount 8 Arg ', eightLetterWord);
+// 	if (randomTruth()) {
+// 		mixedParts.push(eightLetterWord.slice(0, 3));
+// 		mixedParts.push(eightLetterWord.slice(3, 5));
+// 		mixedParts.push(eightLetterWord.slice(5, 8));
+// 	} else {
+// 		mixedParts.push(eightLetterWord.slice(0, 2));
+// 		mixedParts.push(eightLetterWord.slice(2, 5));
+// 		mixedParts.push(eightLetterWord.slice(5, 8));
+// 	}
+// }
+//
+// //** Nine letter word
+// function wordCountNine(nineLetterWord) {
+// 	console.log('wordcount 9 Arg ', nineLetterWord);
+// 	mixedParts.push(nineLetterWord.slice(0, 3));
+// 	mixedParts.push(nineLetterWord.slice(3, 6));
+// 	mixedParts.push(nineLetterWord.slice(6, 9));
+// }
+// wordBreaker(words);
+// console.log('mixedparts ', mixedParts);
+// //** assigning vars to clues
+// //const clues = ['this word has five letters', 'this word has five letters too', 'this word has six letters', 'this word has seven letters', 'this word has eight letters', 'this word has nine letters'];
+// let fiveLetterClue = clues[0];
+// let fiveLetterClueTwo = clues[1];
+// let sixLetterClue = clues[2];
+// let sevenLetterClue = clues[3];
+// let eightLetterClue = clues[4];
+// let nineLetterClue = clues[5];
+//
+// console.log('5 ltr clue => ', fiveLetterClue);
+// console.log('5 ltr clue(2) => ', fiveLetterClueTwo);
+// console.log('6 ltr clue => ', sixLetterClue);
+// console.log('7 ltr clue => ', sevenLetterClue);
+// console.log('8 ltr clue => ', eightLetterClue);
+// console.log('9 ltr clue => ', nineLetterClue);
+//
+// modulusLength(wordClue);
+// console.log('mixedparts ', mixedParts);
+// console.log('mixedClues ', mixedClues);
