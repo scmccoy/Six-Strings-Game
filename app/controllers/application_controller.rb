7e887cb3d85a85d56e91597c67d6f5204ce7d1c4
Adam Reed <kusa.xisa@gmail.com>
2017-03-03 14:15:29 -0500

    Fixed csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :null_session
+
+  # protect_from_forgery with: :null_session
+
+  before_action :update_sanitized_params, if: :devise_controller?
+
+  def update_sanitized_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'users'
+
   resources :puzzles, except: :show
   get 'puzzles/random'
   # get 'index/show'
