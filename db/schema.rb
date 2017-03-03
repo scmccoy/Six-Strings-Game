a24995565c8c7fd68ca949167040b490058cb936
Adam Reed <kusa.xisa@gmail.com>
2017-03-03 16:32:47 -0500

    Added scores

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303192247) do
+ActiveRecord::Schema.define(version: 20170303203832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20170303192247) do
     t.string   "word6"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.integer  "score"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_scores_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +65,5 @@ ActiveRecord::Schema.define(version: 20170303192247) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
   end
 
+  add_foreign_key "scores", "users"
 end
