6961cf715e36f98bcf19c590381734ca91a3d1b0
Vallyre Hyers <vallyre@vallyre.com>
2017-03-04 18:35:11 -0500

    Leaderboard fixes

--- app/assets/javascripts/controllers/leaderboard-controller.js
+++ app/assets/javascripts/controllers/leaderboard-controller.js
@@ -4,14 +4,12 @@
 	ng.module( 'sixStringApp' ).controller( 'LeaderboardController', function( dataService, $q, $state, $scope, localStorageService ) {
 		console.log( 'in LeaderboardController' );
 
-		$scope.getLeaders = function() {
 			$q.when( dataService.get( 'scores' ) ).then( ( response ) => {
 				$scope.leadData = response.data;
+				console.log('responsedata', $scope.leadData);
 			} ).catch( ( error ) => {
 				console.log( error );
 			} );
-
-		};
 
 
 	} );
