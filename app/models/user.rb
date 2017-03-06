d18b62b565456d2f01e999ad99e9cc606660343b
Adam Reed <kusa.xisa@gmail.com>
2017-03-06 16:01:50 -0500

    Changed username and password maximum and minimums

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable
+          :recoverable, :rememberable, :trackable, :validatable,
+          password_length: 1..Float::INFINITY
+          
   include DeviseTokenAuth::Concerns::User
+
+  validates_length_of :username, maximum: 20
 end
